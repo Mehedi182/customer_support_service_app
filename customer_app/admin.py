cddcccb16f9c856d7b70eeb642fcb532d2aafe17
mehedihtt <mehedi.hasan@exos-systems.com>
2025-04-10 19:06:18 +0600

    Update: admin panel header and title updated

--- customer_app/admin.py
+++ customer_app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+
 from .models import HelpRequest
 
-
 admin.site.register(HelpRequest)
+
+
+admin.site.site_header = "Helpdesk Support Admin"
+admin.site.site_title = "Helpdesk Admin Portal"
+admin.site.index_title = "Welcome to the Helpdesk Support System"
